15f5f38a7cde35dccd25836bd031945c62ae6fe8
Dibido <Dibran.Dokter@student.ru.nl>
2019-09-27 15:26:44 +0200

    finished week4

--- assignments/week4/Calculus.hs
+++ assignments/week4/Calculus.hs
@@ -25,18 +25,41 @@ f2 = Id
 f3 = Const 2 :*: Id :*: Id :+: Const 5 :*: Id
 f4 = Const 2 :*: Prim Sin :+: Id
 f5 = Id :*: Prim Cos :.: (Id :*: Id)
+f6 = Const 5 :+: Id
+f7 = Const 0 :*: Id :*: Id :+: Const 5 :*: Id
+f8 = Const 0
 
 applyPrim :: Primitive -> (Double -> Double)
-apply (
+applyPrim (Sin) x = sin x
+applyPrim (Cos) x = cos x
+applyPrim (Exp) x = exp x
 
 apply    :: Function -> (Double -> Double)
-apply (Const r) x = r
+apply (Const r) x = fromRational r
 apply (Id) x = x
-apply (Prim f) x = f x
---apply (f1 :+: f2) x = 
---apply (f1 :*: f2) x = 
---apply (f1 :.: f2) x = 
+apply (Prim f) x = applyPrim f x
+apply (f1 :+: f2) x = (apply f1 x) + (apply f2 x)
+apply (f1 :*: f2) x = (apply f1 x) * (apply f2 x)
+apply (f1 :.: f2) x = apply f1 (apply f2 x)
 
+primDerivative :: Primitive -> Function
+primDerivative (Sin) = Prim Cos
+primDerivative (Cos) = Const (-1) :*: Prim Sin
+primDerivative (Exp) = Prim Exp
 
--- derive   :: Function -> Function
--- simplify :: Function -> Function
+derive   :: Function -> Function
+derive (Const r)    = Const 0
+derive (Id)         = Const 1
+derive (Prim f)     = primDerivative f
+derive (f1 :+: f2)  = (derive f1) :+: (derive f2)
+derive (f1 :*: f2)  = (derive f1) :*: f2 :+: (derive f2) :*: f1
+derive (f1 :.: f2)  = ((derive f1) :.: f2) :*: (derive f2)
+
+simplifiedFunction :: Function -> Function
+simplifiedFunction (Const 0 :+: f2) = simplifiedFunction f2
+simplifiedFunction (f1 :+: Const 0) = simplifiedFunction f1
+simplifiedFunction (Const 0 :*: f2) = Const 0 -- Use maybe?
+simplifiedFunction (f1 :*: Const 0) = Const 0
+simplifiedFunction (f1 :*: f2) = simplifiedFunction f1 :*: simplifiedFunction f2
+simplifiedFunction (f1 :+: f2) = simplifiedFunction f1 :+: simplifiedFunction f2
+simplifiedFunction (Const x) = Const x
