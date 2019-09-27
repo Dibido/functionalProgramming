519caeeb58f25d39f39d1489b963b06d5be45390
Dibran Dokter <hdokter@hg029pc27.science.ru.nl>
2019-09-27 13:29:24 +0200

    updated week4

--- assignments/week4/Calculus.hs
+++ assignments/week4/Calculus.hs
@@ -20,6 +20,23 @@ data Function
   |  Function :.: Function  -- composition of functions
   deriving (Show)
 
--- apply    :: Function -> (Double -> Double)
+f1 = Const 5
+f2 = Id
+f3 = Const 2 :*: Id :*: Id :+: Const 5 :*: Id
+f4 = Const 2 :*: Prim Sin :+: Id
+f5 = Id :*: Prim Cos :.: (Id :*: Id)
+
+applyPrim :: Primitive -> (Double -> Double)
+apply (
+
+apply    :: Function -> (Double -> Double)
+apply (Const r) x = r
+apply (Id) x = x
+apply (Prim f) x = f x
+--apply (f1 :+: f2) x = 
+--apply (f1 :*: f2) x = 
+--apply (f1 :.: f2) x = 
+
+
 -- derive   :: Function -> Function
 -- simplify :: Function -> Function
