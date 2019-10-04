621816084f89c31c3049e8df2b0816d6f97ff58c
Dibido <Dibran.Dokter@student.ru.nl>
2019-10-04 15:16:25 +0200

    updated week5

--- assignments/week5/Unfold.hs
+++ assignments/week5/Unfold.hs
@@ -1,8 +1,9 @@
 module Unfold
 where
 
-import Prelude hiding (take, filter)
+import Prelude hiding (take)
 import qualified Data.List as L
+import Data.Either
 
 unfoldr :: (t -> Maybe (a, t)) -> t -> [a]
 unfoldr rep seed = produce seed
@@ -16,10 +17,22 @@ apo rep seed = produce seed
   where
     produce seed = case rep seed of 
        Left l     -> l
-       Right(a,ns) -> a : produce ns
+       Right(a, new_seed) -> a : produce new_seed
 
 take :: Int -> [a] -> [a]
 take n l = unfoldr (\s -> if (null (fst s)) || (snd s <= 0) then Nothing else Just ((head $ fst s), (tail $ fst s, (snd s)-1))) (l, n)
 
 filter :: (a -> Bool) -> [a] -> [a]
-filter pred l = unfoldr (\x -> case x of [] -> Nothing; x:xs -> if pred x then Just (x, xs) else Nothing) l
+filter pred l = unfoldr (\x -> case x of [] -> Nothing; (x:xs) -> if pred x then Just (x, xs) else Nothing) l 
+
+fibs :: [Integer]
+fibs = unfoldr (\(a,b) -> Just (a, (b,a+b))) (0,1)
+
+notMultipleOf :: Integer -> Integer -> Bool
+notMultipleOf n1 n2 = (n2 `mod` n1) /= 0
+
+primes :: [Integer]
+primes = unfoldr (\(p:xs) -> Just (p, (Prelude.filter (notMultipleOf p) xs))) [2..]
+
+unfoldr' :: (t -> Maybe (a, t)) -> t -> [a]
+unfoldr' rep seed = apo rep [] [seed]
